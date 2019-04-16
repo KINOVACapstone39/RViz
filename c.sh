2710f19d8286d186e79b082086af28ba5d75b767
caelw <33704318+caelw@users.noreply.github.com>
2019-04-16 13:36:44 -0700

    Add files via upload

--- c.sh
+++ c.sh
@@ -2,10 +2,8 @@
 cd catkin_ws
 catkin_make
 cd ../
-gnome-terminal -x bash -c "source ~/catkin_ws/devel/setup.bash; roslaunch kinova_bringup kinova_robot.launch kinova_robotType:=j2n6s300; read -n1"
+gnome-terminal --geometry 25x30+680+2000 -x bash -c "source ~/catkin_ws/devel/setup.bash; roslaunch kinova_bringup kinova_robot.launch kinova_robotType:=j2n6s300; read -n1"
 sleep 3
-gnome-terminal -x bash -c "source ~/catkin_ws/devel/setup.bash; roslaunch j2n6s300_moveit_config j2n6s300_demo.launch; read -n1"
+gnome-terminal --geometry 25x30+680+2000 -x bash -c "source ~/catkin_ws/devel/setup.bash; roslaunch j2n6s300_moveit_config j2n6s300_demo.launch; read -n1"
 sleep 7
-gnome-terminal -x bash -c "source ~/catkin_ws/devel/setup.bash; rosrun kinova_arm_moveit_demo pick_place; read -n1"
-
-
+gnome-terminal --geometry 25x30+680+2000 -x bash -c "source ~/catkin_ws/devel/setup.bash; rosrun kinova_arm_moveit_demo pick_place; read -n1"
