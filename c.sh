5930b4d364bedc1d0793ae7160941a64880decb0
caelw <33704318+caelw@users.noreply.github.com>
2019-04-16 13:38:35 -0700

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd catkin_ws
-catkin_make
-cd ../
+cd catkin_ws #OPTIONAL
+catkin_make #OPTIONAL
+cd ../      #OPTIONAL
 gnome-terminal --geometry 25x30+680+2000 -x bash -c "source ~/catkin_ws/devel/setup.bash; roslaunch kinova_bringup kinova_robot.launch kinova_robotType:=j2n6s300; read -n1"
 sleep 3
 gnome-terminal --geometry 25x30+680+2000 -x bash -c "source ~/catkin_ws/devel/setup.bash; roslaunch j2n6s300_moveit_config j2n6s300_demo.launch; read -n1"
